676efe1985cd99153e91d0538387eeceef7a7388
LuckyFoxCode <sergei181989@gmail.com>
2026-05-20 19:40:34 +0300

    feat(db): expand user schema and add coin history tracking

--- apps/frontend/src/components/UserForm.vue
+++ apps/frontend/src/components/UserForm.vue
@@ -7,6 +7,7 @@ interface User {
 }
 
 const user = ref<User>({ twitchId: '', username: '' });
+const errorMess = ref('');
 
 const createUser = async (userData: User) => {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
@@ -17,39 +18,57 @@ const createUser = async (userData: User) => {
     body: JSON.stringify(userData),
   });
 
-  return response;
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({}));
+
+    const errorMessage = errorData.message || `Ошибка сервера: ${response.status}`;
+    throw new Error(errorMessage);
+  }
+
+  return await response.json();
 };
 
-const onSubmit = () => {
+const onSubmit = async () => {
   const newUser: User = {
     twitchId: user.value.twitchId,
     username: user.value.username,
   };
 
-  console.log(newUser);
-  createUser(newUser);
+  try {
+    errorMess.value = '';
+    await createUser(newUser);
 
-  user.value.twitchId = '';
-  user.value.username = '';
+    user.value.twitchId = '';
+    user.value.username = '';
+  } catch (error) {
+    if (error instanceof Error) {
+      errorMess.value = error.message || 'Что-то пошло не так';
+    }
+  }
+
+  console.log(newUser);
 };
 </script>
 
 <template>
-  <form
-    class="mx-auto flex w-175 flex-col rounded-lg border-2 border-slate-700 bg-slate-800"
-    @submit.prevent="onSubmit"
-  >
-    <h1>User form</h1>
-    <input
-      v-model="user.twitchId"
-      type="text"
-      placeholder="twitchId"
-    />
-    <input
-      v-model="user.username"
-      type="text"
-      placeholder="username"
-    />
-    <button type="submit">Create user</button>
-  </form>
+  <div>
+    <form
+      class="mx-auto flex w-175 flex-col rounded-lg border-2 border-slate-700 bg-slate-800"
+      @submit.prevent="onSubmit"
+    >
+      <h1>User form</h1>
+      <input
+        v-model="user.twitchId"
+        type="text"
+        placeholder="twitchId"
+      />
+      <input
+        v-model="user.username"
+        type="text"
+        placeholder="username"
+      />
+      <button type="submit">Create user</button>
+    </form>
+    <span>{{ errorMess }}</span>
+  </div>
 </template>
