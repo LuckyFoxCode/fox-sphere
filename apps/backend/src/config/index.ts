d8ddfb8389d628d9064873ad45f23f3f33eda3b8
LuckyFoxCode <sergei181989@gmail.com>
2026-05-22 23:18:50 +0300

    chore(backend): update environment configuration and add twitch settings

--- apps/backend/src/config/index.ts
+++ apps/backend/src/config/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
-const getEnv = (key: string, defaultValues?: string): string => {
-  const value = process.env[key] || defaultValues;
+const getEnv = (key: string, defaultValue?: string): string => {
+  const value = process.env[key] || defaultValue;
 
   if (!value) {
     throw new Error(`❌ Ошибка окружения: Переменная ${key} не задана в .env`);
@@ -12,4 +12,12 @@ const getEnv = (key: string, defaultValues?: string): string => {
 export const config = {
   port: Number(process.env.PORT) || 3000,
   databaseUrl: getEnv("DATABASE_URL"),
-};
+
+  twitch: {
+    clientId: getEnv("TWITCH_CLIENT_ID"),
+    channelName: getEnv("TWITCH_CHANNEL_NAME"),
+    clientSecret: getEnv("TWITCH_CLIENT_SECRET"),
+    botAccessToken: getEnv("TWITCH_BOT_ACCESS_TOKEN"),
+    botRefreshToken: getEnv("TWITCH_BOT_REFRESH_TOKEN"),
+  },
+} as const;
