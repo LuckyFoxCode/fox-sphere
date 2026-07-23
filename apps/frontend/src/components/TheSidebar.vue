72ab5de5b5538716749dd79d3cd6db12f451ab7b
LuckyFoxCode <sergei181989@gmail.com>
2026-07-23 14:37:50 +0300

    refactor(style): edit bg and padding for Sidebar component

--- apps/frontend/src/components/TheSidebar.vue
+++ apps/frontend/src/components/TheSidebar.vue
@@ -7,18 +7,16 @@ const { isTimerActive, timer, timeDigits } = useTwitchSocket(socket);
 </script>
 
 <template>
-  <aside class="bg-card/20 flex h-full w-100 flex-col">
-    <div class="flex flex-col">
-      <Transition
-        name="zoom-in"
-        mode="out-in"
-      >
-        <TwitchTimer
-          v-if="isTimerActive && timer"
-          :timer="timer"
-          :time-digits="timeDigits"
-        />
-      </Transition>
-    </div>
+  <aside class="bg-card/70 flex h-full w-100 flex-col pt-1.5 backdrop-blur-md">
+    <Transition
+      name="zoom-in"
+      mode="out-in"
+    >
+      <TwitchTimer
+        v-if="isTimerActive && timer"
+        :timer="timer"
+        :time-digits="timeDigits"
+      />
+    </Transition>
   </aside>
 </template>
