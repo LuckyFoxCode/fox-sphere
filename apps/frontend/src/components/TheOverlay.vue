9331f2a1f82197bf7e69571008496f667949ef01
LuckyFoxCode <sergei181989@gmail.com>
2026-08-08 22:58:54 +0300

    feat(overlay): refactor overlay structure to use TwitchWidgetEventsHub component

--- apps/frontend/src/components/TheOverlay.vue
+++ apps/frontend/src/components/TheOverlay.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { socket } from '@/services';
-import { onUnmounted } from 'vue';
 import { LotteryManager } from './lottery';
-import { PokemonWidget } from './pokemon';
 import TheFooter from './TheFooter.vue';
 import TheHeader from './TheHeader.vue';
 import TheSidebar from './TheSidebar.vue';
-import { TwitchWidget } from './twitch';
-import { UserWidget } from './user';
-
-onUnmounted(() => {
-  socket.disconnect();
-  socket.removeAllListeners();
-});
+import { TwitchWidgetEventsHub } from './widgets-hub';
 </script>
 
 <template>
@@ -20,9 +11,7 @@ onUnmounted(() => {
     <TheHeader />
     <main class="flex w-full flex-1 justify-end">
       <LotteryManager />
-      <PokemonWidget />
-      <TwitchWidget />
-      <UserWidget />
+      <TwitchWidgetEventsHub />
       <TheSidebar />
     </main>
     <TheFooter />
---
+++ apps/frontend/src/components/widgets-hub/TwitchWidgetEventsHub.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { socket } from '@/services';
+import { onUnmounted } from 'vue';
+import { PokemonWidget } from '../pokemon';
+import { TwitchWidget } from '../twitch';
+import { UserWidget } from '../user';
+
+onUnmounted(() => {
+  socket.disconnect();
+  socket.removeAllListeners();
+});
+</script>
+
+<template>
+  <PokemonWidget />
+  <TwitchWidget />
+  <UserWidget />
+</template>
